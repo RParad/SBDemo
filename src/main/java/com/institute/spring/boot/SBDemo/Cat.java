0f1fb2eeb91fa83fd5eea2207a0db74de2c43ca7
Ryan <Ryan2@MacBook-Air.local>
2021-10-27 17:15:49 +0100

    First

--- src/main/java/com/institute/spring/boot/SBDemo/Cat.java
+++ src/main/java/com/institute/spring/boot/SBDemo/Cat.java
@@ -6,7 +6,40 @@ public class Cat {
     private int age;
     private String breed;
 
-    public Cat(String name, int age, String breed){
+    public Cat(String name,int age, String breed){
+            this.name = name;
+            this.age = age;
+            this.breed = breed;
+        }
 
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public int getAge() {
+            return age;
+        }
+
+        public void setAge(int age) {
+            this.age = age;
+        }
+
+        public String getBreed() {
+            return breed;
+        }
+
+        public void setBreed(String breed) {
+            this.breed = breed;
+        }
+
+        public String getJsonObjectOfCat(){
+            return "{"+
+                    "\"name\" : \" " + name + "\"," +
+                    "\" age\" : \" " + age + "\"" +
+                    "}";
     }
 }
--- src/main/java/com/institute/spring/boot/SBDemo/SbDemoApplication.java
+++ src/main/java/com/institute/spring/boot/SBDemo/SbDemoApplication.java
@@ -3,12 +3,21 @@ package com.institute.spring.boot.SBDemo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SbDemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SbDemoApplication.class, args);
 	}
 
+	@GetMapping("/getcat")
+	public String catResponse() {
+		Cat cat = new Cat("T Darg", 7, "Junior Lion");
+
+		return cat.getJsonObjectOfCat();
+	}
 }
+
