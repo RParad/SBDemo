1b6ef51757361d9600f19c49b332c5b579ff4d2b
Ryan <Ryan2@MacBook-Air.local>
2021-10-27 15:48:49 +0100

    SBDemo

---
+++ src/main/java/com/institute/spring/boot/SBDemo/Cat.java
@@ -0,0 +1,12 @@
+package com.institute.spring.boot.SBDemo;
+
+public class Cat {
+
+    private String name;
+    private int age;
+    private String breed;
+
+    public Cat(String name, int age, String breed){
+
+    }
+}
---
+++ src/test/java/com/institute/spring/boot/SBDemo/SbDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.institute.spring.boot.SBDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
